182d3dbfd13c1d99293e6362579895baa1a8d161
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2020-12-24 12:53:56 +0100

    Découverte de container

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
+    protected $calculator;
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+    /**
+     * @Route("/", name="index")
+     */
     public function index()
     {
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         dd("fonctionne");
     }
 
     /**
-     * @Route("{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
      */
 
     public function test(Request $request, $age)
