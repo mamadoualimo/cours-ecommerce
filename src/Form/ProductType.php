43928e574433ac47834735481bb75b0d1065de67
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-02-05 23:52:28 +0100

    les formulaires suite

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,8 +4,13 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Form\Type\PriceType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
+use App\Form\DataTransformer\CentimesTransformer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -28,15 +33,16 @@ class ProductType extends AbstractType
                     'placholder' => 'Tapez une description assez coute mais parlante pour le visiteur'
                 ]
             ])
-            ->add('mainPicture', UrlType::class, [
-                'label' => 'Image du produit',
-                'attr' => ['placeholder' => 'Tapez une URL d\'image !']
-            ])
             ->add('price', MoneyType::class, [
                 'label' => 'Prix du produit',
                 'attr' => [
                     'placeholder' => 'Tapez le prix du produit en €'
-                ]
+                ],
+                'divisor' => 100
+            ])
+            ->add('mainPicture', UrlType::class, [
+                'label' => 'Image du produit',
+                'attr' => ['placeholder' => 'Tapez une URL d\'image !']
             ])
             ->add('category', EntityType::class, [
                 'label' => 'Catégorie',
@@ -46,6 +52,37 @@ class ProductType extends AbstractType
                     return strtoupper($category->getName());
                 }
             ]);
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+        //     $form = $event->getForm();
+
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+
+        // if ($product->getId() === null) {
+        //     $form->add('category', EntityType::class, [
+        //         'label' => 'Catégorie',
+        //         'placeholder' => '-- Choisir une catégorie --',
+        //         'class' => Category::class,
+        //         'choice_label' => function (Category $category) {
+        //             return strtoupper($category->getName());
+        //         }
+        //     ]);
+        // }
+        // });
     }
     public function configureOptions(OptionsResolver $resolver)
     {
