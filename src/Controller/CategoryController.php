f5fb747800678f61110b1cdd0619647c962a7cb5
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-02-17 17:46:49 +0100

    la securité de l'application

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,11 +6,15 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Exception\EnvNotFoundException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -62,9 +66,43 @@ class CategoryController extends AbstractController
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit")
      */
-    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em)
-    {
+    public function edit(
+        $id,
+        CategoryRepository $categoryRepository,
+        Request $request,
+        EntityManagerInterface $em,
+        Security $security
+    ) {
+        // $this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Vous n'avez pas le droit d'accéder à cette ressource");
+        // $user = $security->getUser();
+
+        // if ($user === null) {
+        //     return $this->redirectToRoute('security_login');
+        // }
+
+        // if ($security->isGranted("ROLE_ADMIN") === false) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accéder à cette ressource");
+        // }
+
         $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw new EnvNotFoundException("Cette catégorie n'existe pas");
+        }
+
+        // $security->isGranted('CAN_EDIT', $category);
+
+        // $this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas le propriétaire de cette catégorie");
+
+        // $user = $this->getUser(); // $security->getUser()
+
+        // if (!$user) {
+        //     return $this->redirectToRoute("security_login");
+        // }
+
+        // if ($user !== $category->getOwner()) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le propriétaire de cette catégorie");
+        // }
 
         $form = $this->createForm(CategoryType::class, $category);
 
